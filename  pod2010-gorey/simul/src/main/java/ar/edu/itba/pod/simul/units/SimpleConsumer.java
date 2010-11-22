d9e6c0dfa1318e0a2fa7f7d57174b5995fe4d9bc
cliksmodernos <cliksmodernos@d545f766-f2d0-e1a1-7780-2eb4466dab8b>
2010-11-22 00:24:35 +0000

    Another modification...

---  pod2010-gorey/simul/src/main/java/ar/edu/itba/pod/simul/units/SimpleConsumer.java
+++  pod2010-gorey/simul/src/main/java/ar/edu/itba/pod/simul/units/SimpleConsumer.java
@@ -28,6 +28,7 @@ public class SimpleConsumer extends MarketAgent implements ResourceStock {
 				}
 				waitForResources();
 				waitForWork();
+				amount.set(amount.get() - rate());
 			} catch (InterruptedException e) {
 				// this should happen when finishing or when new sotck arrives
 			}
